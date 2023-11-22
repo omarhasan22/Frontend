818fe031f398b6962deac1d3020187ca9c847699
your <omar.hasan3894@gmail.com>
2023-11-22 02:02:09 +0200

    Change on nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 
 import { AccountService } from './_services';
 import { Account, Role } from './_models';
 
 @Component({ selector: 'app-root', templateUrl: 'app.component.html' })
-export class AppComponent {
+export class AppComponent implements OnInit {
     Role = Role;
     account?: Account | null;
 
@@ -12,7 +12,18 @@ export class AppComponent {
         this.accountService.account.subscribe(x => this.account = x);
     }
 
+    ngOnInit(): void {
+     
+    }
+
     logout() {
         this.accountService.logout();
     }
+
+    isLoggedIn(){
+        if(this.account!=null) return true
+        else return false
+    }
+
+
 }
