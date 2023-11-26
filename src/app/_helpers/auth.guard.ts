28a4e3b7f362feaa708965934e2b2a53eb3a68ce
your <omar.hasan3894@gmail.com>
2023-11-27 01:40:06 +0200

    26112023

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -26,6 +26,6 @@ export class AuthGuard implements CanActivate {
 
         // not logged in so redirect to login page with the return url 
         this.router.navigate(['/account/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+        return true;
     }
 }
