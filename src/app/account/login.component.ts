323a05c549aa14b23024b0370595360a355d754a
your <omar.hasan3894@gmail.com>
2023-12-06 01:45:42 +0200

    61223

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -47,7 +47,7 @@ export class LoginComponent implements OnInit {
                 next: () => {
                     this.submitting = false;
                     // get return url from query parameters or default to home page
-                    localStorage.setItem('login', JSON.stringify(this.f.value));
+                    localStorage.setItem('login', JSON.stringify(this.form.value));
                     const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
                     this.router.navigateByUrl(returnUrl);
                     //this.router.navigate([''], { relativeTo: this.route });
