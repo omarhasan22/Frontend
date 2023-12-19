bc69f8afe0b71fee89cad59b26a4674ef1e79208
your <omar.hasan3894@gmail.com>
2023-12-19 23:37:36 +0200

    191223

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://perfumeserver.onrender.com'
+    apiUrl: 'http://localhost:5000'
 };
+
+// https://perfumeserver.onrender.com
